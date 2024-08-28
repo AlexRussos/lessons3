bad42c462304b342dbfc63a8f263e33ac43a2b7c
AlexRussos <9296562311@bk.ru>
2024-08-28 18:04:43 +0300

    Add files via upload

--- module_1_3.py
+++ module_1_3.py
@@ -2,7 +2,7 @@ name = 'Alex'
 print("Name: " + name)
 age = 36
 print("Age: ", age)
-new_age = age+1
-print("New age:" , new_age)
-is_student = age < new_age
+age = age+1
+print("New age:" , age+1)
+is_student = age == age
 print('Is_student:', is_student)
